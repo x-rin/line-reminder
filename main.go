c27dc9f78faa53107e035e5bb6acf624b0198d97
cappyzawa <m153304w@st.u-gakugei.ac.jp>
2018-01-22 04:17:13 +0900

    enable to check status

--- main.go
+++ main.go
@@ -7,18 +7,20 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-//リクエストに応じたfuncを定義
-func main() {
-	//routingはここ
+func SetupRouter() *gin.Engine {
 	router := gin.Default()
-
 	v1 := router.Group("/api/v1")
 	{
 		v1.POST("reminder", PostReminder)
 		v1.POST("report", PostReport)
-		v1.GET("status", GetStatus)
+		v1.GET("status/:id", GetStatus)
 	}
+	return router
+}
 
+func main() {
+
+	router := SetupRouter()
 	log.Printf("Start Go HTTP Server")
 
 	port := os.Getenv("PORT")
--- message/status.go
+++ message/status.go
@@ -3,9 +3,20 @@ package message
 import (
 	"net/http"
 	"github.com/gin-gonic/gin"
+	"os"
+	"strings"
 )
 
-func GetStatus (c *gin.Context) {
-	message := "Get Status!"
-	c.String(http.StatusOK, "%s", message)
+func GetStatus(c *gin.Context) {
+	target := c.Param("id")
+	status := os.Getenv(strings.ToUpper(target) + "_STATUS")
+
+	if status == "failure" {
+		//TODO: PostReportが実装され次第パラメータをセットしてあげる。
+		PostReport(c)
+	} else {
+		c.JSON(http.StatusOK, gin.H{
+			"status": status,
+		})
+	}
 }
