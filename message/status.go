af6ad9d41fbc08ebf9f2eeefa7e0331c8bb807b5
cappyzawa <m153304w@st.u-gakugei.ac.jp>
2018-01-24 21:30:37 +0900

    enable to pass test

--- message/status.go
+++ message/status.go
@@ -11,17 +11,11 @@ func GetStatus(c *gin.Context) {
 	target := c.Param("id")
 	status := os.Getenv(strings.ToUpper(target) + "_STATUS")
 
-	//if status == "failure" {
-	//	//TODO: PostReportが実装され次第パラメータをセットしてあげる。
-	//	messageType := "hogehoge"
-	//	text := "fugafuga"
-	//} else {
-	//	c.JSON(http.StatusOK, gin.H{
-	//		"status": status,
-	//	})
-	//}
-
-	c.JSON(http.StatusOK, gin.H{
-		"status": status,
-	})
+	if status == "failure" {
+		PostReminder(c)
+	} else {
+		c.JSON(http.StatusOK, gin.H{
+			"status": status,
+		})
+	}
 }
