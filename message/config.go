d92dacd0438d59d70448af99b127bbf2b1aabebb
cappyzawa <m153304w@st.u-gakugei.ac.jp>
2018-01-24 21:35:12 +0900

    remove no needed function

--- message/config.go
+++ message/config.go
@@ -5,15 +5,3 @@ type lineConfig struct {
 	ExpiresIn	int		`json:"expires_in"`
 	TokenType	string	`json:"token_type"`
 }
-
-type LineConfig interface {
-	GetLineConfig(accessToken string, expireIn int, tokenType string) *lineConfig
-}
-
-func GetLineConfig(accessToken string, expireIn int, tokenType string) *lineConfig {
-	return &lineConfig{
-		AccessToken: accessToken,
-		ExpiresIn: expireIn,
-		TokenType: tokenType,
-	}
-}
