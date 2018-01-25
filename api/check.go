9afd4f16449785b1989ca32c14f254a7f6610f88
cappyzawa <m153304w@st.u-gakugei.ac.jp>
2018-01-26 01:06:00 +0900

    enable to reply

--- api/check.go
+++ api/check.go
@@ -3,9 +3,11 @@ package api
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/line/line-bot-sdk-go/linebot"
-	"os"
 	"log"
 	"net/http"
+	"os"
+
+	"encoding/json"
 )
 
 func Check(c *gin.Context) {
@@ -18,7 +20,25 @@ func Check(c *gin.Context) {
 
 	for _, event := range received {
 		log.Println("groupId: " + event.Source.GroupID)
-		log.Println("satuka: " + event.Source.UserID)
+		if event.Source.UserID == os.Getenv("TARGET_ID") {
+
+			textMsg := new(TextMessage)
+			byteMsg, _ := event.Message.MarshalJSON()
+			if err := json.Unmarshal(byteMsg, textMsg); err != nil {
+				//画像メッセージの場合もあるからただエラーを出力するだけにする
+				log.Println(err.Error())
+				c.JSON(http.StatusOK, gin.H{
+					"status": "false",
+				})
+			}
+
+			if textMsg.Text == os.Getenv("REPORT_MESSAGE") {
+				err := PostMessage(os.Getenv("REPLY_SUCCESS"))
+				if err != nil {
+					log.Fatal(err.Error())
+				}
+			}
+		}
 	}
 
 	c.JSON(http.StatusOK, gin.H{
