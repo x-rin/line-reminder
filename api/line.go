183f9e9fc2d1311c58ca4dd7ed81bfe643ac8886
Shu Kutsuzawa <m153304w@st.u-gakugei.ac.jp>
2018-01-26 02:18:20 +0900

    typo修正/type TextMessageの削除 (#13)
    * fix typo
    
    * remove origin TextMessage type

--- api/line.go
+++ api/line.go
@@ -5,11 +5,6 @@ import (
 	"os"
 )
 
-type TextMessage struct {
-	Type	string 	`json: type`
-	Text	string  `json: text`
-}
-
 func PostMessage(message string) error{
 	config := NewLineConfig()
 	bot, err := linebot.New(os.Getenv("CHANNEL_SECRET"), config.AccessToken)
@@ -23,14 +18,14 @@ func PostMessage(message string) error{
 	return nil
 }
 
-func ReplyMessage(token string, messsage string) error {
+func ReplyMessage(token string, message string) error {
 	config := NewLineConfig()
 	bot, err := linebot.New(os.Getenv("CHANNEL_SECRET"), config.AccessToken)
 	if err != nil {
 		return err
 	}
 
-	if _, err := bot.ReplyMessage(token, linebot.NewTextMessage(messsage)).Do(); err != nil {
+	if _, err := bot.ReplyMessage(token, linebot.NewTextMessage(message)).Do(); err != nil {
 		return err
 	}
 	return nil
