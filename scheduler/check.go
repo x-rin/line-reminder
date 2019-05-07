45181400099db374d9ff736fa1dffece27456f3b
Shu Kutsuzawa <cappyzawa@yahoo.ne.jp>
2019-05-07 09:01:45 +0900

    add logger to schedulers (#51)
    * remove reminder config because this is not used
    
    * add logger to scheduler
    
    * add logger to reminder
    
    * change log format to float64 from duration

--- scheduler/check.go
+++ scheduler/check.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/kutsuzawa/line-reminder/factory"
 	"github.com/kutsuzawa/line-reminder/util"
+	"go.uber.org/zap"
 )
 
 // Checker checks status periodically.
@@ -14,13 +15,14 @@ type Checker struct {
 	GroupID  string
 	Duration time.Duration
 
+	Logger         *zap.Logger
 	ServiceFactory factory.ServiceFactory
 }
 
-// Schedule schedulee targets's status periodically
+// Schedule schedules checking targets's status periodically.
 func (c *Checker) Schedule(targets []string) error {
+	c.Logger.Info("check scheduler is started")
 	ticker := time.NewTicker(c.Duration).C
-
 	for {
 		select {
 		case <-ticker:
@@ -35,17 +37,32 @@ func (c *Checker) Schedule(targets []string) error {
 // If status is false, this post message to pager.
 func (c *Checker) Check(targets []string) error {
 	for _, t := range targets {
+		c.Logger.Info("start to check",
+			zap.String("id", t),
+		)
 		status, err := util.GetStatus(t)
 		if err != nil {
+			c.Logger.Error("failed to get status",
+				zap.String("id", t),
+				zap.Error(err),
+			)
 			return err
 		}
 		if !status {
 			lineService, err := c.ServiceFactory.LineService()
 			if err != nil {
+				c.Logger.Error("failed to create line service",
+					zap.String("id", t),
+					zap.Error(err),
+				)
 				return err
 			}
 			name, err := lineService.GetNameByID(t)
 			if err != nil {
+				c.Logger.Error("failed to get name by id",
+					zap.String("id", t),
+					zap.Error(err),
+				)
 				return err
 			}
 
@@ -53,8 +70,15 @@ func (c *Checker) Check(targets []string) error {
 			// Good Morning
 			msg := fmt.Sprintf("To %s\n%s", name, c.Message)
 			if err := lineService.Send(c.GroupID, msg); err != nil {
+				c.Logger.Error("failed to send message for check",
+					zap.String("id", t),
+					zap.Error(err),
+				)
 				return err
 			}
+			c.Logger.Info("cheking is done",
+				zap.String("id", t),
+			)
 		}
 	}
 	return nil
